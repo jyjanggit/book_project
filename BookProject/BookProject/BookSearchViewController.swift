aa1ad87c06d70b2c561d2dde02fb82f0d6be44e8
jyjanggit <jang1305git@gmail.com>
2025-09-02 20:59:04 +0900

    검색 바 추가

--- BookProject/BookProject/BookSearchViewController.swift
+++ BookProject/BookProject/BookSearchViewController.swift
@@ -7,6 +7,44 @@
 
 import UIKit
 
-final class BookSearchViewController: UIViewController {
+final class BookSearchViewController: UIViewController, UISearchBarDelegate, UISearchResultsUpdating {
+  
+  
+  func updateSearchResults(for searchController: UISearchController) {
+  }
+  
+  
+  private var collectionView: UICollectionView!
+  
+  let uISearchController: UISearchController = {
+    let uISearchController = UISearchController(searchResultsController: nil)
+    uISearchController.obscuresBackgroundDuringPresentation = false
+    uISearchController.hidesNavigationBarDuringPresentation = false
+    uISearchController.searchBar.placeholder = "책 제목을 검색하세요"
+    uISearchController.searchBar.returnKeyType = .search
+    return uISearchController
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = UIColor(hex: "#FFFFFF")
+    self.title = "책 검색"
+    setupLayout()
+  }
+  
+  private func setupLayout() {
+    uISearchController.searchResultsUpdater = self
+    uISearchController.searchBar.delegate = self
+    
+    navigationItem.searchController = uISearchController
+    navigationItem.hidesSearchBarWhenScrolling = false
+    
+    definesPresentationContext = true
+  }
+
+  
   
 }
+
+
+
